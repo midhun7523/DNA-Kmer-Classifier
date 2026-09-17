5e88eb72c0ae1e3cd80ce1de10fdabb6e263abbe
Midhun Balaji Perabattula <midhunsssihl@gmail.com>
2026-09-17 15:25:31 +0530

    Update kmer_features.py

--- src/src/kmer_features.py
+++ src/src/kmer_features.py
@@ -1,1 +1,19 @@
+from collections import Counter
 
+
+def get_kmers(sequence, k=3):
+    """
+    Extract overlapping k-mers from a DNA sequence.
+    """
+    return [
+        sequence[i:i+k]
+        for i in range(len(sequence) - k + 1)
+    ]
+
+
+def kmer_frequency(sequence, k=3):
+    """
+    Count the frequency of each k-mer in a DNA sequence.
+    """
+    kmers = get_kmers(sequence, k)
+    return Counter(kmers)
